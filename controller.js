fea68e317881711fe94d7df39c49206de6ef0cfc
Jared <Jared.martin93@Yahoo.com>
2022-05-04 10:49:47 -0500

    add declarations and functions for selected size and color buttons

--- controller.js
+++ controller.js
@@ -2,10 +2,13 @@ const container = document.querySelector(".sketch-container");
 const btn16 = document.querySelector(".x16");
 const btn32 = document.querySelector(".x32");
 const btn64 = document.querySelector(".x64");
-const resetBtn = document.querySelector(".clear-form");
+const resetBtn = document.querySelector(".clear-sketch");
 const colorBtns = document.querySelectorAll('.color-btn');
 
-var currentColor = 'black';
+let currentColor = 'black';
+let selectedSizeBtn = null;
+let selectedColorBtn = document.querySelector('#black');
+selectedColorBtn.className = "color-btn selected";
 
 function applyColor (hoveredDiv, currentColor) {
       hoveredDiv.style.background = currentColor;
@@ -56,10 +59,12 @@ function sizeTo64 () {
     });
 }
 
-function disableSizeButtons (keepLit) {
+function disableSizeButtons (selectedBtn) {
     btn16.disabled = true;
     btn32.disabled = true;    
     btn64.disabled = true;
+    selectedSizeBtn = selectedBtn;
+    selectedSizeBtn.id = "selected";
 }
 
 function enableSizeButtons () {
@@ -68,20 +73,31 @@ function enableSizeButtons () {
     btn64.disabled = false;
 }
 
+function changeSktetchColor (button , color) {
+    // Resets already selected button, then applies attributes to newly selected button
+    if(selectedColorBtn !== null) {
+        selectedColorBtn.className = "color-btn";
+        selectedColorBtn = null;
+    }
+    selectedColorBtn = button;
+    selectedColorBtn.className = "color-btn selected";
+    currentColor = color;
+}
+
 function resetCanvas(parent) {
+    // Reset attributes
+    if(selectedSizeBtn !== null) {
+        selectedSizeBtn.removeAttribute('id');
+        selectedSizeBtn = null;
+    }
     enableSizeButtons();
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
 
-function changeSktetchColor (color) {
-    currentColor = color;
-    console.log(currentColor);
-}
-
 colorBtns.forEach((button) => { button.addEventListener('click', () => {
-    changeSktetchColor(button.id); });
+    changeSktetchColor(button, button.id); });
 });
 btn16.addEventListener('click', () => { sizeTo16() });
 btn32.addEventListener('click', () => { sizeTo32() });
